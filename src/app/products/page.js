12beb005913a448d64d5b10bb947ea6c06d7e14a
Joseph Pal <josephpal27@gmail.com>
2026-05-19 17:36:12 +0530

    Product Listing Added

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,11 @@
 import ProductsBanner from "@/components/products/ProductsBanner";
+import ProductsListing from "@/components/products/ProductsListing";
 
 export default function Products() {
   return (
     <>
       <ProductsBanner />
+      <ProductsListing />
     </>
   );
 }
---
+++ src/components/products/ProductsListing.jsx
@@ -0,0 +1,30 @@
+
+const ProductsListing = () => {
+    return (
+        <section className="
+            pt-[4rem]
+            flex justify-between flex-wrap
+        ">
+
+            {/* Categories */}
+            <div>
+                <span>
+                    Categories
+                </span>
+                <ul>
+                    <li>
+                        Diaries & Notebooks
+                    </li>
+                </ul>
+            </div>
+
+            {/* Products */}
+            <div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default ProductsListing
