577f5cdebdf69dc63b27132ee5aa86d50b39dee8
Joseph Pal <josephpal27@gmail.com>
2026-05-25 12:02:43 +0530

    Blogs Mobile View Added

--- src/components/home/LatestBlogs.jsx
+++ src/components/home/LatestBlogs.jsx
@@ -35,7 +35,7 @@ const LatestBlogs = () => {
                 </h4>
                 <p className="
                     mt-[0.5rem]
-                    text-center w-[70%]
+                    text-center w-full lg:w-[70%]
                 ">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum earum nemo incidunt nesciunt, harum suscipit vitae asperiores quos tenetur dicta. Lorem ipsum dolor sit amet.
                 </p>
@@ -48,36 +48,37 @@ const LatestBlogs = () => {
                 {blogsData.map((blog, index) => {
                     return (
                         <div key={index} className="
-                            w-[31%] bg-primary overflow-hidden
-                            rounded-[40px] sm:rounded-[30px] lg:rounded-[34px] xl:rounded-[37px] 2xl:rounded-[40px]
+                            w-full lg:w-[31%] bg-primary overflow-hidden
+                            rounded-[20px] sm:rounded-[30px] lg:rounded-[34px] xl:rounded-[37px] 2xl:rounded-[40px]
+                            mb-[1rem] lg:mb-0
                         ">
                             <div>
                                 <img
                                     src={blog.image}
                                     alt={blog.title}
                                     loading="lazy"
-                                    className="w-full rounded-b-[40px]"
+                                    className="w-full rounded-b-[20px] lg:rounded-b-[40px]"
                                 />
                             </div>
                             <div className="
-                                p-[2rem] sm:p-[1.8rem] lg:p-[1.6rem] xl:p-[1.8rem] 2xl:p-[2rem]
+                                p-[1rem] sm:p-[1.8rem] lg:p-[1.6rem] xl:p-[1.8rem] 2xl:p-[2rem]
                                 text-white
                             ">
                                 <span className="
-                                    text-[1.2rem] sm:text-[1.2rem] lg:text-[1rem] xl:text-[1.1rem] 2xl:text-[1.2rem] block
+                                    text-[1.1rem] sm:text-[1.2rem] lg:text-[1rem] xl:text-[1.1rem] 2xl:text-[1.2rem] block
                                 ">
                                     {blog.title}
                                 </span>
                                 <p className="
-                                    text-[0.9rem] sm:text-[0.9rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.9rem]
-                                    mt-[0.7rem]
+                                    text-[0.8rem] sm:text-[0.9rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.9rem]
+                                    mt-[0.5rem] lg:mt-[0.7rem]
                                 ">
                                     {blog.desc}
                                 </p>
                                 <div className="
-                                    mt-[1.5rem]
+                                    mt-[0.8rem] lg:mt-[1.5rem]
                                 ">
-                                    <Button label="Read More" color="white" />
+                                    <Button label="Read More" color="white" className="scale-[0.8] lg:scale-[1] ml-[-0.8rem] lg:ml-0" />
                                 </div>
                             </div>
                         </div>
