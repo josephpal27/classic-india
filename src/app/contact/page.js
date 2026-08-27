7c357eb3f42ced70861d01820ed97a1fd8895882
Joseph Pal <josephpal27@gmail.com>
2026-08-27 12:20:48 +0530

    Contact Page Init

---
+++ src/app/contact/page.js
@@ -0,0 +1,14 @@
+import ContactBanner from "@/components/contact/ContactBanner";
+
+export const metadata = {
+  title: "Contact Us - Classic India",
+  description: "",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <ContactBanner />
+    </>
+  );
+}
---
+++ src/components/contact/ContactBanner.jsx
@@ -0,0 +1,36 @@
+
+const ContactBanner = () => {
+    return (
+        <section className="
+            relative px-[1rem] sm:px-[1%]
+        ">
+            <img
+                src="/images/banners/about-banner.avif"
+                alt="About Banner"
+                loading="lazy"
+                className="
+                    w-full h-auto lg:h-[90dvh] object-cover
+                    rounded-[10px] sm:rounded-[25px] lg:rounded-[24px] xl:rounded-[27px] 2xl:rounded-[30px]
+                "
+            />
+            <div className="
+                w-full h-full relative lg:absolute top-0 left-0 flex flex-col justify-end
+                px-0 lg:px-[6%]
+                pb-[1rem] sm:pb-[1.5rem] lg:pb-[5rem] xl:pb-[6rem] 2xl:pb-[7rem]
+                mt-[-3.3rem] lg:mt-0
+            ">
+                <h1 className="text-white pl-[1rem] lg:pl-0">
+                    Contact Us
+                </h1>
+                <p className="
+                    mt-[1.5rem] lg:mt-[1rem]
+                    w-full lg:w-[50%] lg:text-white
+                ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis error consectetur quidem possimus vitae voluptate alias unde eos quia cumque.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default ContactBanner
